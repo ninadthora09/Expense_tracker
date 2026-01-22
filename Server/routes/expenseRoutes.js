30af1a7e79fff99bc3b538b899eaca19d85be505
Ninad Thorat <programmingwithninadthorat4@gmail.com>
2026-01-22 21:15:03 +0530

    DONE

--- Server/routes/expenseRoutes.js
+++ Server/routes/expenseRoutes.js
@@ -1,14 +1,25 @@
 import express from "express";
-import {
-  getExpenses,
-  addExpense,
-  deleteExpense,
-} from "../controllers/expenseController.js";
+import Expense from "../models/Expense.js";
 
 const router = express.Router();
 
-router.get("/", getExpenses);
-router.post("/", addExpense);
-router.delete("/:id", deleteExpense);
+// GET all expenses
+router.get("/", async (req, res) => {
+  const expenses = await Expense.find();
+  res.json(expenses);
+});
+
+// ADD expense
+router.post("/", async (req, res) => {
+  const expense = new Expense(req.body);
+  await expense.save();
+  res.status(201).json(expense);
+});
+
+// DELETE expense
+router.delete("/:id", async (req, res) => {
+  await Expense.findByIdAndDelete(req.params.id);
+  res.json({ message: "Deleted" });
+});
 
 export default router;
